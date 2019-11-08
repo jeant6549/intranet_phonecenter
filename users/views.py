0434b173d60325c3e20b68d2423474a480718ab8
Jean Terrier <jean.terrier@my-digital-school.org>
2019-11-08 16:09:24 +0100

    Settings : create local_settings

--- users/views.py
+++ users/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 # View
-def hello(request):
+def hello_old(request):
 	print("Bonjour")
 	#return JsonResponse({'message':'Hello','price':19.6,})
 	return HttpResponse("Hello World!")
+
+def hello(request):
+	return render(
+		request,
+		'users/hello.html',
+		{
+			'message': "Hello World!",
+		}
+	)
