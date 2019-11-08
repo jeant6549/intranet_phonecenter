ba180a39aa13792733ee619994c4f461f43c19a3
Jean Terrier <jean.terrier@my-digital-school.org>
2019-11-08 14:50:15 +0100

    Hello World : simple version

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-# coding: UTF-8
 from django.urls import path
+from users import views
 
 app_name = 'users'
 urlpatterns = [
+	path(r'hello/', views.hello, name="hello"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+# Import
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 
-# Create your views here.
+# View
+def hello(request):
+	print("Bonjour")
+	#return JsonResponse({'message':'Hello','price':19.6,})
+	return HttpResponse("Hello World!")
